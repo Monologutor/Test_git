0e0bbed7ec6301a7662014f9c107f0f62ac719ca
cyy <2439644676@qq.com>
2020-10-19 14:53:58 +0800

    初始化提交

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Author 马小姐
+ * @Date 2020-10-19 14:53
+ * @Version 1.0
+ * @Description:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你是猪吗");
+    }
+}
